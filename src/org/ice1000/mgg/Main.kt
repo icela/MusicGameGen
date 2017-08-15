be82074bd68b5a42d979f27a0870aeffa62d546b
ice1000 <ice1000kotlin@foxmail.com>
2017-08-16 00:16:17 +0800

    add some pictures

--- src/org/ice1000/mgg/Main.kt
+++ src/org/ice1000/mgg/Main.kt
@@ -2,8 +2,10 @@ package org.ice1000.mgg
 
 import org.frice.game.Game
 import org.frice.game.Game.Companion.launch
+import org.frice.game.resource.image.FileImageResource
 import org.frice.game.utils.audio.play
-import org.frice.game.utils.message.log.FLog
+import org.frice.game.utils.data.Preference
+import org.frice.game.utils.data.string2File
 import org.frice.game.utils.time.Clock
 import org.frice.game.utils.time.FTimeListener
 import java.awt.event.KeyEvent
@@ -19,21 +21,31 @@ val KEYS = mapOf(
 		KeyEvent.VK_SEMICOLON to ";"
 )
 
+val IMAGES = "ASDFJKL;".map { FileImageResource("res/img/VK_$it.png") }
+
 class MusicGame : Game(3) {
 	override fun onInit() {
 		setSize(600, 600)
 		title = "ice1000's music game"
-		addKeyListener(pressed = {
-			if (it.keyCode in KEYS) FLog.v("${Clock.current}: ${KEYS[it.keyCode]}")
-		})
-		addTimeListener(FTimeListener(2000, times = 2, timeUp = {
+		"".string2File("save.db")
+		Preference("save.db").let { database ->
+			addKeyListener(pressed = {
+				if (it.keyCode in KEYS) database.insert("${Clock.current}", "${KEYS[it.keyCode]}")
+			})
+		}
+		addTimeListener(FTimeListener(2000, 2) {
 			play("res/bad-apple.mp3")
-		}))
+		})
 		super.onInit()
 	}
 
 	override fun onLastInit() {
 		super.onLastInit()
+	}
+
+	override fun onExit() {
+		// super.onExit()
+		System.exit(0)
 	}
 }
 
