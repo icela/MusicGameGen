7bde50740a5e0149380b381e193dc6773f08f0ec
ice1000 <ice1000kotlin@foxmail.com>
2017-08-16 00:31:32 +0800

    it works.

--- src/org/ice1000/mgg/Main.kt
+++ src/org/ice1000/mgg/Main.kt
@@ -2,10 +2,13 @@ package org.ice1000.mgg
 
 import org.frice.game.Game
 import org.frice.game.Game.Companion.launch
+import org.frice.game.anim.move.SimpleMove
+import org.frice.game.obj.sub.ImageObject
 import org.frice.game.resource.image.FileImageResource
 import org.frice.game.utils.audio.play
 import org.frice.game.utils.data.Preference
 import org.frice.game.utils.data.string2File
+import org.frice.game.utils.message.FDialog
 import org.frice.game.utils.time.Clock
 import org.frice.game.utils.time.FTimeListener
 import java.awt.event.KeyEvent
@@ -21,26 +24,42 @@ val KEYS = mapOf(
 		KeyEvent.VK_SEMICOLON to ";"
 )
 
-val IMAGES = "ASDFJKL;".map { FileImageResource("res/img/VK_$it.png") }
+val IMAGES = mapOf(*"ASDFJKL;".map { it to FileImageResource("res/img/VK_$it.png") }
+		.toTypedArray())
 
 class MusicGame : Game(3) {
 	override fun onInit() {
 		setSize(600, 600)
 		title = "ice1000's music game"
 		"".string2File("save.db")
-		Preference("save.db").let { database ->
-			addKeyListener(pressed = {
-				if (it.keyCode in KEYS) database.insert("${Clock.current}", "${KEYS[it.keyCode]}")
-			})
-		}
 		addTimeListener(FTimeListener(2000, 2) {
 			play("res/bad-apple.mp3")
 		})
 		super.onInit()
 	}
 
+	override fun onRefresh() {
+		super.onRefresh()
+	}
+
 	override fun onLastInit() {
 		super.onLastInit()
+		when (FDialog(this).confirm("Practise mode?", "Confirm", FDialog.YES_NO_OPTION)) {
+			FDialog.NO_OPTION -> Preference("save.db").let { database ->
+				addKeyListener(pressed = {
+					if (it.keyCode in KEYS) database.insert("${Clock.current}", "${KEYS[it.keyCode]}")
+				})
+			}
+			FDialog.YES_OPTION -> Preference("data.db").list().let {
+				addTimeListener(*it.map {
+					FTimeListener("${it.first}".toInt(), 2, {
+						addObject(2, ImageObject(IMAGES["${it.second}"[0]]!!, x = 236.0, y = -20.0).apply {
+							addAnim(SimpleMove(0, 300))
+						})
+					})
+				}.toTypedArray())
+			}
+		}
 	}
 
 	override fun onExit() {
