ce25a5ac1cb32406689f7284dce5705d6bf2d165
ice1000 <ice1000kotlin@foxmail.com>
2017-08-15 23:35:28 +0800

    update

--- src/org/ice1000/mgg/Main.kt
+++ src/org/ice1000/mgg/Main.kt
@@ -3,17 +3,20 @@ package org.ice1000.mgg
 import org.frice.game.Game
 import org.frice.game.Game.Companion.launch
 import org.frice.game.utils.audio.play
+import org.frice.game.utils.message.log.FLog
+import org.frice.game.utils.time.Clock
+import org.frice.game.utils.time.FTimeListener
 import java.awt.event.KeyEvent
 
-val KEYS = listOf(
-		KeyEvent.VK_A,
-		KeyEvent.VK_S,
-		KeyEvent.VK_D,
-		KeyEvent.VK_F,
-		KeyEvent.VK_J,
-		KeyEvent.VK_K,
-		KeyEvent.VK_L,
-		KeyEvent.VK_SEMICOLON
+val KEYS = mapOf(
+		KeyEvent.VK_A to "A",
+		KeyEvent.VK_S to "S",
+		KeyEvent.VK_D to "D",
+		KeyEvent.VK_F to "F",
+		KeyEvent.VK_J to "J",
+		KeyEvent.VK_K to "K",
+		KeyEvent.VK_L to "L",
+		KeyEvent.VK_SEMICOLON to ";"
 )
 
 class MusicGame : Game(3) {
@@ -21,15 +24,15 @@ class MusicGame : Game(3) {
 		setSize(600, 600)
 		title = "ice1000's music game"
 		addKeyListener(pressed = {
-			if (it.keyCode in KEYS) {
-
-			}
+			if (it.keyCode in KEYS) FLog.v("${Clock.current}: ${KEYS[it.keyCode]}")
 		})
+		addTimeListener(FTimeListener(2000, times = 2, timeUp = {
+			play("res/bad-apple.mp3")
+		}))
 		super.onInit()
 	}
 
 	override fun onLastInit() {
-		play("res/bad-apple.mp3")
 		super.onLastInit()
 	}
 }
