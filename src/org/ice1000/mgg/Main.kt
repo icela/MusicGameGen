faa9680fe1db7e663da5b04333f60b7da973d8e5
ice1000 <ice1000kotlin@foxmail.com>
2017-08-16 00:58:46 +0800

    partially completed

--- src/org/ice1000/mgg/Main.kt
+++ src/org/ice1000/mgg/Main.kt
@@ -1,7 +1,7 @@
 package org.ice1000.mgg
 
 import org.frice.game.Game
-import org.frice.game.Game.Companion.launch
+import org.frice.game.Game.Initializer.launch
 import org.frice.game.anim.move.SimpleMove
 import org.frice.game.obj.sub.ImageObject
 import org.frice.game.resource.image.FileImageResource
@@ -32,7 +32,23 @@ class MusicGame : Game(3) {
 		setSize(600, 600)
 		title = "ice1000's music game"
 		"".string2File("save.db")
-		addTimeListener(FTimeListener(2000, 2) {
+		when (FDialog(this).confirm("Practise mode?", "Confirm", FDialog.YES_NO_OPTION)) {
+			FDialog.NO_OPTION -> Preference("save.db").let { database ->
+				addKeyListener(pressed = {
+					if (it.keyCode in KEYS) database.insert("${Clock.current}", "${KEYS[it.keyCode]}")
+				})
+			}
+			FDialog.YES_OPTION -> Preference("data.db").list().let {
+				addTimeListener(*it.map {
+					FTimeListener("${it.first}".toInt(), 1, {
+						addObject(2, ImageObject(IMAGES["${it.second}"[0]]!!, x = "ASDFJKL;".indexOf("${it.second}"[0]) * 70.0, y = -20.0).apply {
+							addAnim(SimpleMove(0, 300))
+						})
+					})
+				}.toTypedArray())
+			}
+		}
+		addTimeListener(FTimeListener(2000, 1) {
 			play("res/bad-apple.mp3")
 		})
 		super.onInit()
@@ -44,22 +60,6 @@ class MusicGame : Game(3) {
 
 	override fun onLastInit() {
 		super.onLastInit()
-		when (FDialog(this).confirm("Practise mode?", "Confirm", FDialog.YES_NO_OPTION)) {
-			FDialog.NO_OPTION -> Preference("save.db").let { database ->
-				addKeyListener(pressed = {
-					if (it.keyCode in KEYS) database.insert("${Clock.current}", "${KEYS[it.keyCode]}")
-				})
-			}
-			FDialog.YES_OPTION -> Preference("data.db").list().let {
-				addTimeListener(*it.map {
-					FTimeListener("${it.first}".toInt(), 2, {
-						addObject(2, ImageObject(IMAGES["${it.second}"[0]]!!, x = 236.0, y = -20.0).apply {
-							addAnim(SimpleMove("ASDFJKL;".indexOf("${it.second}"[0]) * 70, 300))
-						})
-					})
-				}.toTypedArray())
-			}
-		}
 	}
 
 	override fun onExit() {
