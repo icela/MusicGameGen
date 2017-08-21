5ad0aba9e003a7d38307c50068478922fcfdc208
ice1000 <ice1000kotlin@foxmail.com>
2017-08-22 01:29:19 +0800

    almost done

--- src/org/ice1000/mgg/PlayerPlay.kt
+++ src/org/ice1000/mgg/PlayerPlay.kt
@@ -1,6 +1,5 @@
 package org.ice1000.mgg
 
-import org.frice.game.anim.move.AccelerateMove
 import org.frice.game.anim.move.SimpleMove
 import org.frice.game.obj.sub.ImageObject
 import org.frice.game.obj.sub.ShapeObject
@@ -14,11 +13,11 @@ fun MusicGame.player() = Preference("data.db").list().let {
 	val res = ColorResource.BLUE
 	val resultImages = listOf("Perfect", "Good", "Bad", "Miss").map { ImageResource.fromPath("res/img/$it.png") }
 	val wall = ShapeObject(ColorResource.COLORLESS, FRectangle(700, 10), -10.0, 590.0)
-	addObject(1, ShapeObject(res, FRectangle(700, 10), -10.0, 450.0), ShapeObject(res, FRectangle(700, 10), -10.0, 520.0), wall)
+	addObject(1, ShapeObject(res, FRectangle(700, 10), -10.0, 450.0), ShapeObject(res, FRectangle(700, 10), -10.0, 540.0), wall)
 	addTimeListener(*it.map {
-		FTimeListener("${it.first}".toInt() + 2200, 1, {
+		FTimeListener("${it.first}".toInt() + 2600, 1, {
 			addObject(2, ImageObject(IMAGES["${it.second}"[0]]!!, x = pos(it.second) * 70.0, y = -20.0).apply {
-				addAnim(SimpleMove(0, 800))
+				addAnim(SimpleMove(0, 500))
 				addCollider(wall, { if (this.res !in resultImages) this.res = resultImages.last() })
 			})
 		})
@@ -28,7 +27,7 @@ fun MusicGame.player() = Preference("data.db").list().let {
 			println("${KEYS[it.keyCode]}, ${layers[2].objects.size}")
 			layers[2].objects.forEach { obj ->
 				if ((obj as ImageObject).res !in resultImages && IMAGES[KEYS[it.keyCode]!![0]] == obj.res) when (obj.y) {
-					in 400..510 -> {
+					in 400..500 -> {
 						obj.res = resultImages[0]
 						println("${obj.y}")
 					}
